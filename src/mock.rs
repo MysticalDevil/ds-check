a3cbcc01a4d917f0bcd21fc416e1dc64f0c76134
MysticalDevil <jinpeng7981@outlook.com>
2026-05-05 10:23:21 +0800

    refactor: use api::USAGE_PROMPT constant in mock_day_tokens

--- src/mock.rs
+++ src/mock.rs
@@ -67,7 +67,7 @@ fn model_usage_totals(days: &[api::DayUsage], model: &str) -> api::ModelUsage {
     model_usage(
         model,
         &[
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, &hit.to_string()),
             (api::USAGE_PROMPT_CACHE_MISS, &miss.to_string()),
             (api::USAGE_RESPONSE, &response.to_string()),
@@ -143,7 +143,7 @@ fn empty_model(model: &str) -> api::ModelUsage {
     model_usage(
         model,
         &[
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "0"),
             (api::USAGE_PROMPT_CACHE_MISS, "0"),
             (api::USAGE_RESPONSE, "0"),
@@ -155,35 +155,35 @@ fn empty_model(model: &str) -> api::ModelUsage {
 fn mock_day_tokens(day: u32) -> Vec<(&'static str, &'static str)> {
     match day {
         1 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "1235072"),
             (api::USAGE_PROMPT_CACHE_MISS, "259591"),
             (api::USAGE_RESPONSE, "33927"),
             (api::USAGE_REQUEST, "37"),
         ],
         2 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "45691008"),
             (api::USAGE_PROMPT_CACHE_MISS, "944444"),
             (api::USAGE_RESPONSE, "284599"),
             (api::USAGE_REQUEST, "432"),
         ],
         3 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "12284672"),
             (api::USAGE_PROMPT_CACHE_MISS, "70253"),
             (api::USAGE_RESPONSE, "132405"),
             (api::USAGE_REQUEST, "130"),
         ],
         4 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "21178880"),
             (api::USAGE_PROMPT_CACHE_MISS, "311968"),
             (api::USAGE_RESPONSE, "138126"),
             (api::USAGE_REQUEST, "186"),
         ],
         _ => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "0"),
             (api::USAGE_PROMPT_CACHE_MISS, "0"),
             (api::USAGE_RESPONSE, "0"),
@@ -195,35 +195,35 @@ fn mock_day_tokens(day: u32) -> Vec<(&'static str, &'static str)> {
 fn mock_flash_day_tokens(day: u32) -> Vec<(&'static str, &'static str)> {
     match day {
         1 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "512000"),
             (api::USAGE_PROMPT_CACHE_MISS, "128000"),
             (api::USAGE_RESPONSE, "45000"),
             (api::USAGE_REQUEST, "120"),
         ],
         2 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "2048000"),
             (api::USAGE_PROMPT_CACHE_MISS, "512000"),
             (api::USAGE_RESPONSE, "180000"),
             (api::USAGE_REQUEST, "480"),
         ],
         3 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "1024000"),
             (api::USAGE_PROMPT_CACHE_MISS, "256000"),
             (api::USAGE_RESPONSE, "90000"),
             (api::USAGE_REQUEST, "240"),
         ],
         4 => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "4096000"),
             (api::USAGE_PROMPT_CACHE_MISS, "1024000"),
             (api::USAGE_RESPONSE, "360000"),
             (api::USAGE_REQUEST, "960"),
         ],
         _ => vec![
-            ("PROMPT_TOKEN", "0"),
+            (api::USAGE_PROMPT, "0"),
             (api::USAGE_PROMPT_CACHE_HIT, "0"),
             (api::USAGE_PROMPT_CACHE_MISS, "0"),
             (api::USAGE_RESPONSE, "0"),
